0e37ae23b9438d8f207c7339ab21acd1acc3b83a
bertram wegener <eaa24bkw@students.eaaa.dk>
2025-01-22 14:46:56 +0100

    name and age

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let firstName = `Bertram`;
+// console.log(`Mit navn er ${firstName}`);
+
+let lastName = `Wegener`;
+// console.log(`Mit efternavn er ${lastName}`);
+
+let age = 24;
+// console.log(`Min alder er ${age}`);
+
+console.log(`Hi my name is ${firstName} ${lastName}, I'm ${age} years old!`);
